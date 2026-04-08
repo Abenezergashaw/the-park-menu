63488c1f30002c0a480a6fa2aff75f11d956a53e
cherent tsegaye <abenigas59@gmail.com>
2026-04-08 15:57:51 +0300

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/home.vue";
-import Admin from "../views/admin.vue";
+import Home from "../views/Home.vue";
+import Admin from "../views/Admin.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
